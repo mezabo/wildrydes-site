954202dbcaf39057ca237b675850719ece4b854a
mezabo <106002876+mezabo@users.noreply.github.com>
2024-10-26 09:14:11 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_mlDuXYVn5', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '641g68b6r1ua7hd16urf98pfbq', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: '' // e.g. us-east-1
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
